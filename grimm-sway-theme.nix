75b642e16073586226f486f142000723be735f04
LordGrimmauld <soeren@benjos.de>
2023-11-26 15:32:22 +0100

    fix nix-build

---
+++ grimm-sway-theme.nix
@@ -0,0 +1,39 @@
+{ lib
+, stdenv
+, fetchFromGitHub
+}:
+
+# { pkgs ? import <nixpkgs> {} }:
+
+stdenv.mkDerivation rec {
+  pname = "grimm-sway-themes";
+  version = "0.1.0";
+
+  doCheck = true;
+#  dontUnpack = true;
+
+  src = ./.;
+
+  installPhase = ''
+    # $out is an automatically generated filepath by nix,
+    # but it's up to you to make it what you need. We'll create a directory at
+    # that filepath, then copy our sources into it.
+    mkdir $out
+    cp -rv ./src/* $out
+  '';
+
+  # passthru.tests.version =
+  #  testVersion { package = grimm-sway-themes; };
+
+  meta = with lib; {
+    description = "My theme and config for sway + waybar";
+    longDescription = ''
+      this is a collection of relevant config files needed for sway and waybar
+    '';
+    homepage = "https://github.com/LordGrimmauld/grimm-sway-config";
+    changelog = "https://github.com/LordGrimmauld/grimm-sway-config/commits/master";
+    license = licenses.mit;
+    maintainers = [ maintainers.grimmauld ];
+    platforms = platforms.all;
+  };
+}
