583cedc623231473fb18a7a7b7637940e47afc55
LordGrimmauld <soeren@benjos.de>
2023-11-27 11:51:51 +0100

    testing

--- grimm-sway-theme.nix
+++ grimm-sway-theme.nix
@@ -1,4 +1,5 @@
 { lib
+, pkgs
 , stdenv
 , fetchFromGitHub
 }:
@@ -12,15 +13,32 @@ stdenv.mkDerivation rec {
   doCheck = true;
 #  dontUnpack = true;
 
+  nativeBuildInputs = with pkgs; [
+    hello
+    waybar
+    coreutils
+    makeWrapper
+  ];
+
+
   src = ./.;
 
   installPhase = ''
     # $out is an automatically generated filepath by nix,
     # but it's up to you to make it what you need. We'll create a directory at
     # that filepath, then copy our sources into it.
-    mkdir $out
-    cp -rv ./src/* $out
+    mkdir -p $out/bin
+    echo "outpath (install) is $out"
+    echo "${grimm-sway-themes}"
+    cp -rv ./src/* $out # */ comment to break the line comment thingy, super annoying
+    cp ${pkgs.writeScript "test-res" ''echo "out path (runtime) $out"'' } $out/bin/test-res
+    cp ${pkgs.writeScript "sway-with-config" "sway --unsupported-gpu" } $out/bin/sway-with-config
   '';
+
+  postFixup = ''
+          echo "outpath is $out"
+          wrapProgram $out/bin/sway-with-config --set PATH ${lib.makeBinPath [ pkgs.swayfx pkgs.waybar pkgs.hello ]}
+        '';
 
   # passthru.tests.version =
   #  testVersion { package = grimm-sway-themes; };
